54f7a7b58a3c9b266e81117687a3e0e4083573f7
aarushinarapureddy <aarushinarapureddy2006@gmail.com>
2026-08-28 18:45:09 +0530

    feat: implement user registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-def home(request):
-    return render(request, "home.html")
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
