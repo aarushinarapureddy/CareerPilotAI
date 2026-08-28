011e3feb9bf955c0a5d31b51cefcea1d61ab2b0d
aarushinarapureddy <aarushinarapureddy2006@gmail.com>
2026-08-28 18:46:47 +0530

    feat: add registration route

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("register/", views.register, name="register"),
 ]
